c377740935b61444d0435a6b0b9e80e97a44ca5a
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-08-18 15:32:46 -0400

    remove div's

--- app/cheatsheets/page.tsx
+++ app/cheatsheets/page.tsx
@@ -4,38 +4,36 @@ export default function CheatSheetPage() {
   return (
     <>
       <h2>Cheat Sheets</h2>
-      <div>
-        <p>Quickly find all the class names and CSS properties.</p>
-        <div className="mt-8">
-          <p className="mb-2">Websites</p>
-          <ul>
-            <li>
-              <Link href="https://tailwindcomponents.com/cheatsheet/">
-                Option 1
-              </Link>{' '}
-              (more up to date)
-            </li>
-            <li>
-              <Link href="https://umeshmk.github.io/Tailwindcss-cheatsheet/">
-                Option 2
-              </Link>{' '}
-              (more visual)
-            </li>
-          </ul>
-          <p className="mb-2 mt-6">Browser extensions:</p>
-          <ul>
-            <li>
-              <Link href="https://chrome.google.com/webstore/detail/tailwind-cheat-sheet-exte/jmggagicdeomaenmjjlphbamdaeojcol">
-                Quick reference
-              </Link>
-            </li>
-            <li>
-              <Link href="https://chrome.google.com/webstore/detail/gimli-tailwind/fojckembkmaoehhmkiomebhkcengcljl">
-                Dev Tools
-              </Link>
-            </li>
-          </ul>
-        </div>
+      <p>Quickly find all the class names and CSS properties.</p>
+      <div className="mt-8">
+        <p className="mb-2">Websites</p>
+        <ul>
+          <li>
+            <Link href="https://tailwindcomponents.com/cheatsheet/">
+              Option 1
+            </Link>{' '}
+            (more up to date)
+          </li>
+          <li>
+            <Link href="https://umeshmk.github.io/Tailwindcss-cheatsheet/">
+              Option 2
+            </Link>{' '}
+            (more visual)
+          </li>
+        </ul>
+        <p className="mb-2 mt-6">Browser extensions:</p>
+        <ul>
+          <li>
+            <Link href="https://chrome.google.com/webstore/detail/tailwind-cheat-sheet-exte/jmggagicdeomaenmjjlphbamdaeojcol">
+              Quick reference
+            </Link>
+          </li>
+          <li>
+            <Link href="https://chrome.google.com/webstore/detail/gimli-tailwind/fojckembkmaoehhmkiomebhkcengcljl">
+              Dev Tools
+            </Link>
+          </li>
+        </ul>
       </div>
     </>
   );
--- app/preflight/page.tsx
+++ app/preflight/page.tsx
@@ -4,27 +4,25 @@ export default function PreflightPage() {
   return (
     <>
       <h2>Preflight</h2>
-      <div>
-        <p>An opinionated set of base styles for Tailwind projects.</p>
-        <ul className="mt-8">
-          <li>
-            <Link href="https://tailwindcss.com/docs/preflight">
-              Preflight docs
-            </Link>
-          </li>
-          <li>
-            <Link href="https://github.com/tailwindlabs/tailwindcss/blob/master/src/css/preflight.css">
-              Code on GitHub
-            </Link>
-          </li>
-          <li>
-            <Link href="https://www.natestephens.dev/unstyled-lists-role-attribute#if-using-tailwind-css">
-              A note
-            </Link>{' '}
-            about the <code>role</code> attribute.
-          </li>
-        </ul>
-      </div>
+      <p>An opinionated set of base styles for Tailwind projects.</p>
+      <ul className="mt-8">
+        <li>
+          <Link href="https://tailwindcss.com/docs/preflight">
+            Preflight docs
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/tailwindlabs/tailwindcss/blob/master/src/css/preflight.css">
+            Code on GitHub
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.natestephens.dev/unstyled-lists-role-attribute#if-using-tailwind-css">
+            A note
+          </Link>{' '}
+          about the <code>role</code> attribute.
+        </li>
+      </ul>
     </>
   );
 }
