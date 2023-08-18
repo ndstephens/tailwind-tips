f1cced44ef35bcd8d53f581d59220b2dc5d24f30
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-08-18 16:51:41 -0400

    remove div

--- app/custommodifiers/page.tsx
+++ app/custommodifiers/page.tsx
@@ -4,31 +4,29 @@ export default function CustomModifiersPage() {
   return (
     <>
       <h2>Custom Modifiers</h2>
-      <div>
-        <p>
-          Register your own custom modifiers that can be used just like built-in
-          variants.
-        </p>
-        <ul className="mt-8">
-          <li>
-            <Link href="https://tailwindcss.com/docs/plugins#static-variants">
-              Static Variants
-            </Link>
-          </li>
-          <li>
-            <Link href="https://tailwindcss.com/docs/plugins#parent-and-sibling-states">
-              Parent and Sibling States
-            </Link>
-          </li>
-          <li>
-            <code>hocusv</code> example in the{' '}
-            <Link href="https://github.com/ndstephens/tailwind-tips/blob/main/tailwind.config.ts">
-              tailwind.config.js
-            </Link>{' '}
-            file of this project
-          </li>
-        </ul>
-      </div>
+      <p>
+        Register your own custom modifiers that can be used just like built-in
+        variants.
+      </p>
+      <ul className="mt-8">
+        <li>
+          <Link href="https://tailwindcss.com/docs/plugins#static-variants">
+            Static Variants
+          </Link>
+        </li>
+        <li>
+          <Link href="https://tailwindcss.com/docs/plugins#parent-and-sibling-states">
+            Parent and Sibling States
+          </Link>
+        </li>
+        <li>
+          <code>hocusv</code> example in the{' '}
+          <Link href="https://github.com/ndstephens/tailwind-tips/blob/main/tailwind.config.ts">
+            tailwind.config.js
+          </Link>{' '}
+          file of this project
+        </li>
+      </ul>
     </>
   );
 }
